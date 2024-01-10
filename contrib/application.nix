8ce2c52a80964ab8fd0fb727e4303235d1d9b4bd
Evan Mattiza <emattiza@gmail.com>
2024-01-10 14:19:48 +0000

    fixup derivation, add ignores

--- contrib/application.nix
+++ contrib/application.nix
@@ -4,4 +4,13 @@ python3Packages.buildPythonApplication {
   pyproject = true;
   version = "0.2.0";
   pname = "oshit";
+  nativeBuildInputs = with python3Packages; [
+    setuptools
+  ];
+  propagatedBuildInputs = with python3Packages; [
+    textual
+    httpx
+    humanize
+    xdg-base-dirs
+  ];
 }
--- contrib/flake.nix
+++ contrib/flake.nix
@@ -6,11 +6,14 @@
   }: let
     supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-  in
-    forAllSystems (
-      system: {
-        packages.default = pkgs.callPackages ./application.nix;
+  in {
+    packages = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        callPackage = pkgs.callPackage;
+      in {
+        default = callPackage ./application.nix {};
       }
     );
+  };
 }
